57e058dd48b599ae4083484453f360ec4b35949c
Alex Cusimayta <alexgabrielcusimaytalobo@gmail.com>
2025-07-26 01:30:26 -0500

    Agregando modificaciones en carpeta js

---
+++ js/01.js
@@ -0,0 +1,11 @@
+//Const son variables que no pueden ser nodificadas
+const precio = 30
+console.log(precio)
+
+//Si intentamos modificar una variable const
+const cliente = "Alessio"
+cliente = 30
+
+//imprime error ya que cliente no puede ser modificado
+console.log(cliente)
+
