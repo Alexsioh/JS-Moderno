40866a43005ee1adf23544d129d9a0c5e1387081
Alex Cusimayta <alexgabrielcusimaytalobo@gmail.com>
2025-07-26 01:20:28 -0500

    Creacion de archivo variable-let y 01

---
+++ js/variable-let.js
@@ -0,0 +1,15 @@
+//Las variables let pueden cambiar su valor
+
+//pueden inicializar con o sin valor
+let cliente 
+
+//pueden cambiar su valor
+cliente = "Alex"
+cliente = 30
+//imprime 30 en la consola
+console.log(cliente)
+
+//inicializamos una variable pero no le agregamos un valor
+let precio
+//Imprime undefined (Inicializado pero no definido)
+console.log(precio)
